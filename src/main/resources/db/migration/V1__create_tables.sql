d5dc445222ada12c91287984dcc611d8fa2ee5d9
TQ <756342530@qq.com>
2022-01-26 14:30:13 +0800

    使用docker和mysql

--- src/main/resources/db/migration/V1__create_tables.sql
+++ src/main/resources/db/migration/V1__create_tables.sql
@@ -1,19 +1,19 @@
-create table news
+create table NEWS
 (
     id          bigint primary key auto_increment,
     title       text,
     content     text,
     url         varchar(800),
-    created_at  timestamp,
-    modified_at timestamp
+    created_at  timestamp default now(),
+    modified_at timestamp default now()
 );
 
 create table LINK_TO_BE_PROCESSED
 (
-    link varchar(800),
+    link varchar(800)
 );
 
 create table LINK_ALREADY_PROCESSED
 (
-    link varchar(800),
+    link varchar(800)
 )
